a2a10d199d0bf6cd87cdca4b1c157f9e2fef84ce
Malo Grall <malo.grall@gmail.com>
2022-09-12 20:58:09 +0200

    Building TS files + Tried with CSS

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,34 @@
 /**
  * @type {import('rollup').RollupOptions}
  */
-import typescript from '@rollup/plugin-typescript';
+import typescript from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
 
-const config = {
-  plugins: [typescript()],
-  input: 'src/**.ts',
+const config = [{
+  plugins: [
+    typescript(),
+    postcss({
+      extensions: ['.css']
+    })
+  ],
+  input: 'src/background.ts',
   output: {
-    dir: 'dist'
+    dir: 'dist/',
+    format: 'cjs'
   }
-};
-export default config;
+}, {
+  plugins: [typescript()],
+  input: 'src/options.ts',
+  output: {
+    dir: 'dist/',
+    format: 'cjs'
+  }
+}, {
+  plugins: [typescript()],
+  input: 'src/popup.ts',
+  output: {
+    dir: 'dist/',
+    format: 'cjs'
+  }
+}]
+export default config
