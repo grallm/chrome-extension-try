6190dc5d612d531a3afb4f14c02c05ac50906029
Malo Grall <malo.grall@gmail.com>
2022-09-13 10:38:58 +0200

    Fetching all pages form tabs

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,60 @@
+const stychPages = [
+  {
+    id: '8/116',
+    title: 'Règles de circulation',
+    link: 'https://www.stych.fr/elearning/course/8/116',
+    parent: null
+  },
+  {
+    id: '8/117',
+    title: 'Conducteur - conductrice',
+    link: 'https://www.stych.fr/elearning/course/8/117',
+    parent: null
+  },
+  {
+    id: '8/118',
+    title: 'Autres usagers',
+    link: 'https://www.stych.fr/elearning/course/8/118',
+    parent: null
+  },
+  {
+    id: '8/120',
+    title: 'Réglementation',
+    link: 'https://www.stych.fr/elearning/course/8/120',
+    parent: null
+  },
+  {
+    id: '8/119',
+    title: 'Circuler à moto',
+    link: 'https://www.stych.fr/elearning/course/8/119',
+    parent: null
+  },
+  {
+    id: '8/121',
+    title: 'Porter secours',
+    link: 'https://www.stych.fr/elearning/course/8/121',
+    parent: null
+  },
+  {
+    id: '8/122',
+    title: 'Élements mécaniques moto',
+    link: 'https://www.stych.fr/elearning/course/8/122',
+    parent: null
+  },
+  {
+    id: '8/123',
+    title: 'Protection et sécurité',
+    link: 'https://www.stych.fr/elearning/course/8/123',
+    parent: null
+  },
+  {
+    id: '8/124',
+    title: 'Éco-conduite',
+    link: 'https://www.stych.fr/elearning/course/8/124',
+    parent: null
+  }
+]
+
 const color = '#3aa757'
 
 chrome.runtime.onInstalled.addListener(() => {
--- src/popup.ts
+++ src/popup.ts
@@ -1,26 +1,53 @@
-// Initialize button with user's preferred color
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const changeColor = document.getElementById('changeColor')!
 
-chrome.storage.sync.get('color', ({ color }) => {
-  changeColor.style.backgroundColor = color
-})
+interface PageEntry {
+  id: string
+  title: string,
+  link: string,
+  parent: string
+}
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-  // console.log(tab)
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id as any },
-    func: setPageBackgroundColor
+  const tabs = await chrome.tabs.query({
+    url: 'https://www.stych.fr/*'
   })
+
+  // Loop all tabs and query pages
+  const tabExecPromises: Promise<chrome.scripting.InjectionResult<PageEntry[]>[]>[] = []
+  for (const tab of tabs) {
+    if (tab.id) {
+      tabExecPromises.push(chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: getPagesFromTab
+      }))
+    }
+  }
+
+  // Extract only the result from the promise: page method results
+  const result = (await Promise.all(tabExecPromises)).reduce((acc, tabResult) => {
+    return [...acc, ...tabResult[0].result]
+  }, [] as PageEntry[])
+
+  console.log(result)
 })
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor () {
-  chrome.storage.sync.get('color', ({ color }) => {
-    document.body.style.backgroundColor = color
+/**
+ * Get all page children from a tab
+ */
+const getPagesFromTab = (): PageEntry[] => {
+  const root: PageEntry[] = []
+  document.querySelectorAll('#sheets .item-title').forEach(e => {
+    if (e) {
+      root.push({
+        id: e.textContent?.trim() || '',
+        title: e.textContent?.trim() || '',
+        link: (e as HTMLAnchorElement).href,
+        parent: ''
+      })
+    }
   })
+  console.log('root')
+  return root
 }
